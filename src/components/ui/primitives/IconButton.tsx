8325740b09504d77f7ec4d61dff3bd92ce88903c
softdevx15 <yucheng.jin25@outlook.com>
2025-09-05 16:10:00 +0200

    fix: update IconButton Button import

--- src/components/ui/primitives/IconButton.tsx
+++ src/components/ui/primitives/IconButton.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { cn } from "@/lib/utils";
-import { buttonSizes, type ButtonSize } from "./button";
+import { buttonSizes, type ButtonSize } from "./Button";
 
 type Icon = "xs" | "sm" | "md" | "lg";
 
