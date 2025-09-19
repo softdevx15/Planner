d1dfb962a2077c7d17f595944757e492e74e54c3
softdevx15 <yucheng.jin25@outlook.com>
2025-09-19 02:38:57 +0200

    Align hero actions with grid columns

--- src/components/home/WelcomeHeroFigure.tsx
+++ src/components/home/WelcomeHeroFigure.tsx
@@ -41,7 +41,7 @@ const haloSecondaryStyle: React.CSSProperties = {
 };
 
 const defaultSizes =
-  "(max-width: 639px) 100vw, (max-width: 1023px) 33vw, 40vw";
+  "(max-width: 767px) 100vw, (max-width: 1023px) calc(100vw / 3), calc(100vw * 5 / 12)";
 
 export interface WelcomeHeroFigureProps {
   className?: string;
