09dedd6c5802b0cec6e5600ad4da6ef7c95d1050
softdevx15 <yucheng.jin25@outlook.com>
2025-09-21 19:32:14 +0200

    Fix client-only animated select imports

--- src/components/ui/layout/hero/HeroGlitchStyles.tsx
+++ src/components/ui/layout/hero/HeroGlitchStyles.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 // src/components/ui/layout/hero/HeroGlitchStyles.tsx
 
 import * as React from "react";
--- src/components/ui/select/AnimatedSelectList.tsx
+++ src/components/ui/select/AnimatedSelectList.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Check } from "lucide-react";
