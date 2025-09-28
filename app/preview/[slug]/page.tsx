766eaf0f5cc039e12ba0d823319cf68911c27cf7
softdevx15 <yucheng.jin25@outlook.com>
2025-09-28 02:54:54 +0200

    Fix Next preview exports

--- app/preview/[slug]/page.tsx
+++ app/preview/[slug]/page.tsx
@@ -1,8 +1,13 @@
-export {
-  dynamic,
-  dynamicParams,
-  generateStaticParams,
-  generateMetadata,
+import PreviewPage, {
+  dynamic as previewDynamic,
+  dynamicParams as previewDynamicParams,
+  generateMetadata as previewGenerateMetadata,
+  generateStaticParams as previewGenerateStaticParams,
 } from "../../../src/app/preview/[slug]/page";
 
-export { default } from "../../../src/app/preview/[slug]/page";
+export default PreviewPage;
+
+export const dynamic = previewDynamic;
+export const dynamicParams = previewDynamicParams;
+export const generateStaticParams = previewGenerateStaticParams;
+export const generateMetadata = previewGenerateMetadata;
